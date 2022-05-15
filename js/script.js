d97257d7e3a8a544b1b605cf3a311c9eecbd6868
rifianfernando19 <rifian.fernando@binus.ac.id>
2022-05-15 19:13:01 +0700

    add information template

--- js/script.js
+++ js/script.js
@@ -154,3 +154,26 @@ function check(){
         radio5.classList.remove("enable");
     }
 }
+const join = async () => {
+	const email = document.querySelector("#email").value;
+    let valid = true;
+    if(email.length == 0){
+        alert("Email must be filled");
+        return;
+    }
+    if(email.includes("@") == false){
+        alert("Email must contain @");
+        return
+    }
+    if(email.includes(".") == false){
+        alert("Email must contain .");
+        return
+    }
+    if(email.indexOf(" ") >= 0){
+        alert("Email cannot contain spaces");
+        return
+    }
+    if(valid == true){
+        alert("We will send our latest news to your email");
+    }
+};
